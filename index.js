8db620f565c1981ab265c5aa019d33257d24f679
khusil <khushilmanji@yahoo.com>
2023-07-24 12:31:52 -0400

    added code to index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./util/generateMarkdown");
+
+const questions = [
+    {
+
+    
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
